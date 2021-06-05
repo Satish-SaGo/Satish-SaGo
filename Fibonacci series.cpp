08684c3f8d80c5c5f7a9be6fe0b657c808d18e0a
ICONICSTAR <84264252+Satish-SaGo@users.noreply.github.com>
2021-06-05 15:33:29 +0530

    Add files via upload

---
+++ Fibonacci series.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+ {  
+  int n1=0,n2=1,n3,num;    
+ cout<<"Enter the number: ";    
+ cin>>num;    
+ cout<<n1<<" "<<n2<<" ";     
+ for(int i=2;i<num;++i) //num+1-->0 1 1 2 3 5
+ {    
+  n3=n1+n2;    
+  cout<<n3<<" ";    
+  n1=n2;    
+  n2=n3;    
+ }    
+   return 0;  
+   }  
